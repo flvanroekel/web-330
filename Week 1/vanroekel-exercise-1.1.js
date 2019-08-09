16c64c0544d8d1a36d8a574e429618ff36a28087
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-08-08 19:39:47 -0500

    vanroekel-exercise-1.1.js

---
+++ Week 1/vanroekel-exercise-1.1.js
@@ -0,0 +1,56 @@
+const header = require('../Vanroekel-header.js')
+                       
+console.log(header.display("Faye", "Van Roekel", "Exercise 1.1"));
+
+
+/*
+============================================
+; Title:  Exercise 1.1 
+; Author: Faye Van Roekel
+; Date:   08 August 2019
+; Description: Class Refresher - Exercise 1.1
+;===========================================
+*/
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 1.1
+  Today's Date
+
+  -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+
+*/
+// start program
+
+function CellPhone(manufacturer, model, color, price)
+{
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+
+    this.getPrice = function()
+        {
+            return this.price; 
+        }
+
+    this.getModel = function()
+        {
+            return this.model;
+        }        
+
+    this.getDetails = function ()
+        {
+            return "Manufacturer: " + this.manufacturer + "\nModel: " +  this.getModel() + 
+                "\nColor: " + this.color + "\nPrice: " + this.getPrice();
+        }
+}
+var cellPhone = new CellPhone("Google", "Pixel 2XL", "Black", "799.99");
+console.log(cellPhone.getDetails()); 
+
+    
